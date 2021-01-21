90a723236dc00a4fe7649a9bf04f6a64a2673cde
fendysketsa <fendysketsa@gmail.com>
2021-01-21 15:34:24 +0700

    update timezone

--- config/app.php
+++ config/app.php
@@ -66,7 +66,7 @@ return [
     |
      */
 
-    'timezone' => env('TIME_ZONE'),
+    'timezone' => env('TIME_ZONE', 'Asia/Jakarta'),
 
     /*
     |--------------------------------------------------------------------------
