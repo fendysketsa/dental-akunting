82539dae9d767baefda4186d03e4142d39c8796a
fendysketsa <fendysketsa@gmail.com>
2021-01-24 23:03:15 +0700

    update kernel last fix

--- app/Http/Middleware/HttpsProtocolMiddleware.php
+++ app/Http/Middleware/HttpsProtocolMiddleware.php
@@ -15,6 +15,10 @@ class HttpsProtocolMiddleware
      */
     public function handle($request, Closure $next)
     {
+        if (!$request->secure() && app()->environment('production')) {
+            return redirect()->secure($request->getRequestUri());
+        }
+
         return $next($request);
     }
 }
